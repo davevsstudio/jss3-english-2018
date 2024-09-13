27beca0bf41783350d65b54a5d1a48ca9721d7a5
davestar <142616037+davevsstudio@users.noreply.github.com>
2024-09-12 22:17:06 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,301 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "According to the passage, indiscipline is seen as ",
+    answer: "mondernity",
+    options: [
+      "mondernity",
+      "development",
+      "influence",
+      "determination"
+    ]
+  },
+    {
+    numb: 2,
+    question: "One of the major things that determine the development of a country is ______",
+    answer: "technology",
+    options: [
+      "society",
+      "indiscipline",
+      "phenomena",
+      "technology"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The acts of indiscipline start from",
+    answer: "home",
+    options: [
+      "modernity",
+      "technology",
+      "home",
+      "society"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Indiscipline among youths does NOT result in",
+    answer: "integrity",
+    options: [
+      "drug abuse",
+      "secret cults",
+      "rebellion",
+      "integrity"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The major influuence in one's life is ______ responsibility",
+    answer: "parental",
+    options: [
+      "parental",
+      "technology",
+      "societal",
+      "environmental"
+    ]
+  },
+  {
+    numb: 6,
+    question: "According to the passage, a myth is also a",
+    answer: "story",
+    options: [
+      "play",
+      "story",
+      "game",
+      "drama"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "From the passage, we can infer that",
+    answer: "every human community has myths",
+    options: [
+      "every human community has myths",
+      "groups have myths",
+      "africians do not have myths",
+      "No one has myths"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "The writer's opinion that myths are tied to _____ and _____ beliefs",
+    answer: "cultural and social",
+    options: [
+      "education and reglious",
+      "cultural and social",
+      "occupation and marital",
+      "religious and cultural"
+    ]
+  },
+  {
+    numb: 9,
+    question: "A title suitable for the passage is",
+    answer: "myths",
+    options: [
+      "african myths",
+      "myths",
+      "non-afircan myths",
+      "traditional myths"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Myths that relate to creation and supernatural powers are ______ myths",
+    answer: "painting",
+    options: [
+      "ceramics design",
+      "painting",
+      "textile design",
+      "graphic design"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Scinece and technology have assited the world in many ways through wonderful _________",
+    answer: "inventions",
+    options: [
+      "inventions",
+      "interventions",
+      "intentation",
+      "creations"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Virtually every nook and cranny of the world has felt the _______ of science and technology.",
+    answer: "impart",
+    options: [
+      "impart",
+      "feelings",
+      "impacts",
+      "actions"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "In the hospital, the ______ thermomater can be",
+    answer: "clinical",
+    options: [
+      "medical",
+      "mercury",
+      "technical",
+      "clinical"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "used to mesure the ______ of the body",
+    answer: "temperature",
+    options: [
+      "temperature",
+      "warmth",
+      "coldness",
+      "hotness"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Doctors use a ______ to listen to the heart beats",
+    answer: "stethoscope",
+    options: [
+      "testhoscope",
+      "strecthoscope",
+      "stethoscope",
+      "microscope"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "My mother and my sister _____ travelling to Kano next week",
+    answer: "are",
+    options: [
+      "is",
+      "was",
+      "are",
+      "am"
+    ]
+  },
+
+
+  {
+    numb: 17,
+    question: "When you _____ will you please help me?",
+    answer: "finish",
+    options: [
+      "finishes",
+      "had finished",
+      "finish",
+      "are finished"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "The train will leave ___ six o'clock exactly",
+    answer: "at",
+    options: [
+      "on",
+      "at",
+      "by",
+      "in"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Jane will continue to get low marks _____ she works harder",
+    answer: "unless",
+    options: [
+      "however",
+      "although",
+      "unless",
+      "even"
+    ]
+  },
+
+
+
+  {
+    numb: 20,
+    question: "You will be in school tomorrow ____ you?",
+    answer: "won't you",
+    options: [
+      "won't you",
+      "will",
+      "would",
+      "don't"
+    ]
+  },
+
+
+  {
+    numb: 21,
+    question: "My father has bought a",
+    answer: "brand new red car",
+    options: [
+      "brand red new car",
+      "brand new red car",
+      "red new brand car",
+      "new brand red car"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "______ my father came to our school he has not talked to me",
+    answer: "after",
+    options: [
+      "since",
+      "but",
+      "after",
+      "although"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Neither the boys ____ the girls could answer the question",
+    answer: "nor",
+    options: [
+      "or",
+      "and",
+      "nor",
+      "both"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Tom climbed the stairs and ____ to bed",
+    answer: "went",
+    options: [
+      "go",
+      "went",
+      "going",
+      "goes"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "The counsellor warned the students to _____ from such behaviour in the future",
+    answer: "desist",
+    options: [
+      "resist",
+      "retreat",
+      "desist",
+      "denounce"
+    ]
+  },
+];
